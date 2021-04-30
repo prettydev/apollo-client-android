96c93c30b68119f710920e9afdb17e1f98473690
prettydev <creditcoder@hotmail.com>
2021-04-30 10:29:42 +0800

    add test schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,30 +35,33 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
 
-//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-//    kotlinOptions {
-//        jvmTarget = "1.8"
-//    }
-//}
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
 
 dependencies {
     val room_version = "2.2.6"
     val kotlin_version = "1.4.32"
 
     kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-runtime:$room_version")   
-    implementation("androidx.room:room-ktx:$room_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-    implementation("androidx.core:core-ktx:1.3.2")
+    
+    implementation("androidx.room:room-runtime:$room_version")   
+    implementation("androidx.room:room-ktx:$room_version")    
     implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.0-rc01")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.0-rc01")
     implementation("androidx.paging:paging-runtime-ktx:2.1.2")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.security:security-crypto:1.0.0-rc02")
+
+    implementation("com.google.android.material:material:1.3.0")    
     implementation("com.apollographql.apollo:apollo-runtime:2.5.5")
     implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.5")
     implementation("com.theartofdev.edmodo:android-image-cropper:2.8.+")
--- readme.md
+++ readme.md
@@ -1,8 +1,15 @@
 # Commands
 
+## kotlin based gradle build
+
 ```
 
 ./gradlew init --dsl kotlin
 
 ```
+
+
+# References
+
+https://github.com/gradle/kotlin-dsl-samples
 
